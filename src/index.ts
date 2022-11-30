b3b4957cd1ae3034d1ea623c2919a6e59e6ea6a1
Szymon Antczak <szymon0antczak@gmail.com>
2022-11-30 15:28:38 +0100

    File structure chang

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@ export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 
+export * from './events/types/custom-event';
+export * from './events/types/subjects';
 export * from './events/types/wallet-created-event';
 export * from './events/types/wallet-updated-event';
 export * from './events/listener';
-export * from './events/types/custom-event';
 export * from './events/publisher';
-export * from './events/subjects';
